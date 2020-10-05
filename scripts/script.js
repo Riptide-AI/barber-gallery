f7ca52f8b78e50afcfca2a2038722d226e7948d3
Riptide-AI <54863875+Riptide-AI@users.noreply.github.com>
2020-10-06 00:45:16 +0300

    alpha 1.3.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,7 @@
 'use strict';
-
-let prev = 0;
-window.onscroll = function (prev){
+window.onscroll = function (){
    const nav = document.querySelector(".navbar");
    let top = window.scrollY;
-   console.log(top);
    if(top >=100){
       nav.classList.add("active");
    }
@@ -17,5 +14,43 @@ window.onscroll = function (prev){
    else{
       nav.classList.remove("underline");
    }
-   return prev
+}
+function smoothScroll(Element) {
+
+   Element = document.getElementById(Element);
+   let selectedPosX = 0;
+   let selectedPosY = 0;
+   while (Element != null) {
+       selectedPosX += Element.offsetLeft;
+       selectedPosY += Element.offsetTop;
+       Element = Element.offsetParent;
+   }
+   window.scrollTo(selectedPosX, selectedPosY);
+}
+
+function sliderScroll(element, parentId) {
+   element = document.getElementById(element);
+   parentId = document.getElementById(parentId);
+   const elementList = document.querySelectorAll('.bar')
+   const slider = document.querySelector(".slider-block");
+   let posX = element.offsetLeft;
+   let posY = element.offsetTop;
+   slider.scroll(posX, posY);
+   eachElementRemoveClass("remove", "active", elementList);
+   parentId.classList.add("active");
+}
+function eachElementRemoveClass(operation,status, elementList){
+   switch(operation) {
+      case ("add"):
+         for(let i = 0; i < elementList.length; i++){
+            elementList[i].classList.add(status);
+         }
+         break;
+      case ("remove"):
+         for(let i = 0; i < elementList.length; i++){
+            elementList[i].classList.remove(status);
+         }
+         break;
+   }
+   
 }
